8362ca912bb0fe58a0d869dffa43f5128bdb72fd
DevoidBark2 <petros_9002@mail.ru>
2023-02-18 21:48:04 +0300

    Create Models tables

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 const express = require('express');
-const sequelize = require('./db.js')
-
+const sequelize = require('./db.js');
+const models = require('./models/models.js');
+const cors = require('cors');
 const PORT = process.env.PORT || 5000
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const start = async () =>{
     try{
